be1c25b03486444db0fe05513a4703b359bcae54
kjw4025 <82869542+kjw4025@users.noreply.github.com>
2025-04-02 13:51:23 +0900

    초기설정
    빌드완료(db없음)

---
+++ src/main/java/com/npchat/ainpc/AiNpcApplication.java
@@ -0,0 +1,13 @@
+package com.npchat.ainpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiNpcApplication.class, args);
+	}
+
+}
